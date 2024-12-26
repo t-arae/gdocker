8021a5a1dd437d193f832a1b2e79200fefd91d55
t-arae <aroearue@gmail.com>
2024-12-26 17:22:55 +0900

    feat: update to use interactive mode

--- util.go
+++ util.go
@@ -2,44 +2,27 @@ package main
 
 import (
 	"bufio"
-	"cmp"
-	"fmt"
 	"log/slog"
 	"os"
-	"path/filepath"
-	"slices"
 	"strings"
 )
 
-func DirLs(dir string) []string {
-	files, err := os.ReadDir(dir)
+func isDir(path string) bool {
+	info, err := os.Stat(path)
 	if err != nil {
-		panic(err)
+		return false
+	} else {
+		return info.IsDir()
 	}
-
-	var paths []string
-	for _, file := range files {
-		if file.IsDir() {
-			if file.Name() == "archive" {
-				continue
-			}
-			paths = append(paths, DirLs(filepath.Join(dir, file.Name()))...)
-			continue
-		}
-		paths = append(paths, filepath.Join(dir, file.Name()))
-	}
-	return paths
 }
 
-func findDockerfile(dir string) []string {
-	files := DirLs(dir)
-	var dockerfiles []string
-	for _, file := range files {
-		if filepath.Base(file) == "Dockerfile" {
-			dockerfiles = append(dockerfiles, file)
-		}
+func isFile(path string) bool {
+	info, err := os.Stat(path)
+	if err != nil {
+		return false
+	} else {
+		return !info.IsDir()
 	}
-	return dockerfiles
 }
 
 func findLines(path string, prefix string) []string {
@@ -65,63 +48,4 @@ func findLines(path string, prefix string) []string {
 	}
 
 	return results
-}
-
-// Dockerfileを読み込んで、FROM命令から依存しているDockerImageを返す
-func readDockerfile(dfile string) []DockerImage {
-	from_directives := findLines(dfile, "FROM")
-	var deps []DockerImage
-	for _, v := range from_directives {
-		li := strings.Split(strings.TrimPrefix(v, "FROM "), " ")
-		if strings.HasPrefix(li[0], "--platform=") {
-			deps = append(deps, NewDockerImage(li[1]))
-		} else {
-			deps = append(deps, NewDockerImage(li[0]))
-		}
-	}
-	return deps
-}
-
-// Makefileを読み込んで、latest tag依存しているDockerImageを返す
-func readMakefile(mfile string) DockerImage {
-	img_name := strings.TrimPrefix(findLines(mfile, "IMG_NAME = ")[0], "IMG_NAME = ")
-	latest_ver := strings.TrimPrefix(findLines(mfile, "LATEST_VERSION = ")[0], "LATEST_VERSION = ")
-	return NewDockerImage(fmt.Sprintf("%s:%s", img_name, latest_ver))
-}
-
-// Dockerfileのパスのスライスを受け取って、Dependencyのスライスを返す
-func findDependencyFromDockerfiles(dfiles []string) []Dependency {
-	var deps []Dependency
-	for _, dfile := range dfiles {
-		dd := filepath.Dir(dfile)
-		dddd := filepath.Dir(dd)
-
-		// Dockerfileから読み取った依存関係を追加
-		left := NewDockerImage(fmt.Sprintf("%s:%s", filepath.Base(dddd), filepath.Base(dd)))
-		for _, right := range readDockerfile(dfile) {
-			if left.String() == right.String() {
-				continue
-			}
-			deps = append(deps, Dependency{left, right})
-		}
-
-		// latest tagの依存関係を追加
-		left = NewDockerImage(fmt.Sprintf("%s:%s", filepath.Base(dddd), "latest"))
-		right := readMakefile(filepath.Join(dddd, "Makefile"))
-		if left.String() == right.String() {
-			continue
-		}
-		deps = append(deps, Dependency{left, right})
-	}
-
-	// 重複した依存関係を除く
-	slices.SortFunc(deps, func(a, b Dependency) int {
-		if n := cmp.Compare(a.From.String(), b.From.String()); n != 0 {
-			return n
-		}
-		return cmp.Compare(a.To.String(), b.To.String())
-	})
-	deps = slices.Compact(deps)
-
-	return deps
 }
