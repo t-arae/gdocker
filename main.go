c1899b02f18cd18d95397875c72aef4a0d7d4ae5
t-arae <toshihiro.arae@gmail.com>
2025-10-20 17:24:50 +0900

    bump version

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 var (
 	APP_NAME    = "gdocker"
 	APP_USAGE   = "personal docker wrapper tool written in Go"
-	APP_VERSION = "0.0.3"
+	APP_VERSION = "0.0.4"
 )
 
 func cmdMain() *cli.Command {
